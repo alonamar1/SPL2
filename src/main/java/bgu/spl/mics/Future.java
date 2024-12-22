c62b2ae99e87c272a6b0efa197937218344055aa
alonamar <alonamar24@gmail.com>
2024-12-22 20:19:36 +0200

    messagebusstart

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -3,105 +3,101 @@ package bgu.spl.mics;
 import java.util.concurrent.TimeUnit;
 
 /**
- * A Future object represents a promised result - an object that will
- * eventually be resolved to hold a result of some operation. The class allows
- * Retrieving the result once it is available.
- * 
- * Only private methods may be added to this class.
- * No public constructor is allowed except for the empty constructor.
+ * A Future object represents a promised result - an object that will eventually
+ * be resolved to hold a result of some operation. The class allows Retrieving
+ * the result once it is available.
+ *
+ * Only private methods may be added to this class. No public constructor is
+ * allowed except for the empty constructor.
  */
 public class Future<T> {
-	
-	private boolean available;
-	private T result;
-	/**
-	 * This should be the the only public constructor in this class.
-	 */
-	public Future() {
-		this.result = null;
-		this.available = false;
-	}
-	/*private Future(T result)
+
+    private boolean available;
+    private T result;
+
+    /**
+     * This should be the the only public constructor in this class.
+     */
+    public Future() {
+        this.result = null;
+        this.available = false;
+    }
+
+    /*private Future(T result)
 	{
 		this.result = result;
 		this.isDone = true;
 	}
-	*/
-	
-	/**
+     */
+    /**
      * retrieves the result the Future object holds if it has been resolved.
      * This is a blocking method! It waits for the computation in case it has
      * not been completed.
      * <p>
-     * @return return the result of type T if it is available, if not wait until it is available.
-     * 	       
+     * @return return the result of type T if it is available, if not wait until
+     * it is available.
+     *
      */
-	public synchronized T get() {
-		
-		while (!available)
-		{
-			try{
-				this.wait();
-			}
-			catch(InterruptedException e){
-				Thread.currentThread().interrupt();
-			}
-		}
-		return this.result;
-		
-	}
-	
-	/**
+    public synchronized T get() {
+
+        while (!available) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        return this.result;
+
+    }
+
+    /**
      * Resolves the result of this Future object.
      */
-	public synchronized void resolve (T result) {
-		//TODO: implement this.
-		this.result = result;
-		available = true;
-		this.notifyAll();
-	}
-	
-	/**
+    public synchronized void resolve(T result) {
+        //TODO: implement this.
+        this.result = result;
+        available = true;
+        this.notifyAll();
+    }
+
+    /**
      * @return true if this object has been resolved, false otherwise
      */
-	public boolean isDone() {
-		//TODO: implement this.
-		return this.available;
-	}
-	
-	/**
+    public boolean isDone() {
+        //TODO: implement this.
+        return this.available;
+    }
+
+    /**
      * retrieves the result the Future object holds if it has been resolved,
      * This method is non-blocking, it has a limited amount of time determined
      * by {@code timeout}
      * <p>
-     * @param timout 	the maximal amount of time units to wait for the result.
-     * @param unit		the {@link TimeUnit} time units to wait.
-     * @return return the result of type T if it is available, if not, 
-     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
-     *         elapsed, return null.
+     * @param timout the maximal amount of time units to wait for the result.
+     * @param unit	the {@link TimeUnit} time units to wait.
+     * @return return the result of type T if it is available, if not, wait for
+     * {@code timeout} TimeUnits {@code unit}. If time has elapsed, return null.
      */
-	public synchronized T get(long timeout, TimeUnit unit) { //try catch?? לחשוב על הכל שוב
-		
-		long toWait = unit.toMillis(timeout); //convert the time to wait to lont in miliseconds 
-		long startTime = System.currentTimeMillis(); //checks the global time
-		long remainTime = toWait;
-		while (remainTime > 0) //while time hasn't finish
-		{
-			if (available) {
-				return result;
-			}
-			try{
-				this.wait(remainTime);
-			}
-			catch(InterruptedException e){
-				Thread.currentThread().interrupt();
-			}
-			long passedTime = System.currentTimeMillis() - startTime; //checks how huch time 
-			remainTime = toWait - passedTime;
+    public synchronized T get(long timeout, TimeUnit unit) { //try catch?? לחשוב על הכל שוב
 
-		}
-		return null;
-	}
+        long toWait = unit.toMillis(timeout); //convert the time to wait to lont in miliseconds 
+        long startTime = System.currentTimeMillis(); //checks the global time
+        long remainTime = toWait;
+        while (remainTime > 0) //while time hasn't finish
+        {
+            if (available) {
+                return result;
+            }
+            try {
+                this.wait(remainTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            long passedTime = System.currentTimeMillis() - startTime; //checks how huch time 
+            remainTime = toWait - passedTime;
+
+        }
+        return null;
+    }
 }
-
-
--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,5 +1,19 @@
 package bgu.spl.mics;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Vector;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.sun.java.swing.plaf.motif.resources.motif_zh_CN;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
@@ -7,55 +21,95 @@ package bgu.spl.mics;
  */
 public class MessageBusImpl implements MessageBus {
 
-	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
+    private static MessageBusImpl instance;
 
-	}
+    // Mapping of MicroServices to their message queues
+    private final Map<MicroService, BlockingQueue<Message>> messageQueues;
 
-	@Override
-	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+    // Event subscriptions
+    private final Map<Class<? extends Event>, List<MicroService>> eventSubscribers;
 
-	}
+    // Broadcast subscriptions
+    private final Map<Class<? extends Broadcast>, List<MicroService>> broadcastSubscribers;
 
-	@Override
-	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
+    private static final Object lock = new Object();
 
-	}
+    private MessageBusImpl() {
+        messageQueues = new ConcurrentHashMap<>();
+        eventSubscribers = new ConcurrentHashMap<>();
+        broadcastSubscribers = new ConcurrentHashMap<>();
+    }
 
-	@Override
-	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
+    /* public static MessageBusImpl getInstance() {
+        
+        try {
+            if (instance == null) {
+                instance = new MessageBusImpl();
+            }
+        } finally {
+            lock.unlock();
+        }
+        return instance;
+    } */
+    @Override
+    public synchronized <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
 
-	}
+        //add new event if needed, and than add m to the matched event
+        eventSubscribers.putIfAbsent(type, new CopyOnWriteArrayList<MicroService>());//Thread-safe variant of arrayList
+        eventSubscribers.get(type).add(m);
+    }
 
-	
-	@Override
-	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public synchronized void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
 
-	@Override
-	public void register(MicroService m) {
-		// TODO Auto-generated method stub
+        //add new Broadcast if needed, and than add m to the matched Broadcast
+        broadcastSubscribers.putIfAbsent(type, new CopyOnWriteArrayList<>());
+        broadcastSubscribers.get(type).add(m);
 
-	}
+    }
 
-	@Override
-	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
+    @Override
+    public <T> void complete(Event<T> e, T result) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public Message awaitMessage(MicroService m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void sendBroadcast(Broadcast b) {
+        // TODO Auto-generated method stub
 
-	
+    }
 
+    @Override
+    /*  public <T> Future<T> sendEvent(Event<T> e) {
+
+        List<MicroService> subscribers = eventSubscribers.getOrDefault(e.getClass(), new CopyOnWriteArrayList<MicroService>());
+        if (subscribers.isEmpty()) {
+            return null;
+        }
+        return null;
+
+    }
+     */
+
+    @Override
+    public synchronized void register(MicroService m) {
+
+        messageQueues.putIfAbsent(m, new LinkedBlockingQueue<>());
+
+    }
+
+    @Override
+    public void unregister(MicroService m) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Message awaitMessage(MicroService m) throws InterruptedException {
+
+        return messageQueues.get(m).take(); 
+		//since blockingQueue is a thread safe class, we don't need "wait()" method.
+		//in addition, it deals with interupts. 
+    }
 }
