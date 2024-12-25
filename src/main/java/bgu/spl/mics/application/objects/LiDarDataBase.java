1fe1e1d4dc5dc17e1c4e0353f1a5ee5e8c8ae0a2
RanIvgi <ranivgi1@gmail.com>
2024-12-25 15:46:07 +0200

    GsonUpdate

--- src/main/java/bgu/spl/mics/application/objects/LiDarDataBase.java
+++ src/main/java/bgu/spl/mics/application/objects/LiDarDataBase.java
@@ -1,6 +1,10 @@
 package bgu.spl.mics.application.objects;
 import java.util.List;
 
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * LiDarDataBase is a singleton class responsible for managing LiDAR data.
  * It provides access to cloud point data and other relevant information for tracked objects.
