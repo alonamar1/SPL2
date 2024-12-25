08d73d4c58abad4c8b147b9fa14c742591139c52
RanIvgi <ranivgi1@gmail.com>
2024-12-25 19:59:24 +0200

    startLanMark&Fusion

--- src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
+++ src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
@@ -1,13 +1,55 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.List;
+
 /**
- * Manages the fusion of sensor data for simultaneous localization and mapping (SLAM).
- * Combines data from multiple sensors (e.g., LiDAR, camera) to build and update a global map.
- * Implements the Singleton pattern to ensure a single instance of FusionSlam exists.
+ * Manages the fusion of sensor data for simultaneous localization and mapping
+ * (SLAM).
+ * Combines data from multiple sensors (e.g., LiDAR, camera) to build and update
+ * a global map.
+ * Implements the Singleton pattern to ensure a single instance of FusionSlam
+ * exists.
  */
 public class FusionSlam {
+
+    private LandMark[] landmarks; // צריך לבדוק האם זה מערך או רשימה
+    private List<Pose> poses;
+
+    private FusionSlam() {
+        landmarks = new LandMark[0]; // 
+        poses = null;
+    }
+
+    /**
+     * Returns the single instance of FusionSlam.
+     *
+     * @return The single instance of FusionSlam.
+     */
+
+    public static FusionSlam getInstance() {
+        return FusionSlamHolder.getInstance();
+    }   
+    
+
+    public LandMark[] getLandmarks() {
+        return landmarks;
+    }   
+
+    public List<Pose> getPoses() {
+        return poses;
+    }   
+
+    
     // Singleton instance holder
     private static class FusionSlamHolder {
-        // TODO: Implement singleton instance logic.
+        private static FusionSlam instance;
+
+        private FusionSlamHolder() {
+            instance = new FusionSlam();    
+        }   
+
+        public static FusionSlam getInstance() {
+            return instance;
+        }
     }
 }
