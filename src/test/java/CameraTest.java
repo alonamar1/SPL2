2e88888fd7d009bf8ad06eb31e7ff1c45c34f1ef
alonamar <alonamar24@gmail.com>
2025-01-02 14:07:22 +0200

    cameratest

--- src/test/java/CameraTest.java
+++ src/test/java/CameraTest.java
@@ -39,7 +39,7 @@ public class CameraTest {
         if (i%2==0) {camera1.get(i-1).addDetectedObject(new DetectedObject(id1 + "1", id1));}
         if (i%3==0) {
             camera1.get(i-1).addDetectedObject(new DetectedObject(id2 + "2", id2));
-            camera1.get(i-1).addDetectedObject(new DetectedObject(id3 + i, id3)); }
+            camera1.get(i-1).addDetectedObject(new DetectedObject(id3 + "1", id3)); }
         if (i%4==0) { 
             camera1.get(i-1).addDetectedObject(new DetectedObject(id4 + "1", id4));
             camera1.get(i-1).addDetectedObject(new DetectedObject(id5 + "1", id5));}
@@ -77,24 +77,95 @@ public class CameraTest {
 
     }
 
+    public List<StampedDetectedObjects> camera2Test()
+    { 
+    List<StampedDetectedObjects> camera2 = new LinkedList<>();
+    String id1="wall";
+    String id2= "door";
+    String id3="chair";
+    String id4 = "sofa";
+    String id5= "TV";
+    String id6 = "carpet";
+    String id7 = "bin";
+    String id8 = "bed";
+    String id9 = "fan";
+    String id10 = "computer";
+    String id11 = "dog";
+
+    camera2.add(new StampedDetectedObjects(1));
+    camera2.get(0).addDetectedObject(new DetectedObject(id11 + "1", id11));
+
+    for (int i=2; i<=12; i++)
+    {
+        camera2.add(new StampedDetectedObjects(i));
+        if (i%2==0) {camera2.get(i-1).addDetectedObject(new DetectedObject(id5 + "1", id5));}
+        if (i%3==0) {
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id7 + "2", id7));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id3 + i, id3)); }
+        if (i%4==0) { 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id5 + "2", id5));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id5 + "1", id5));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id1 + "2", id1));
+        }
+        if (i%5==0) { 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id5 + "2", id5));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id6 + "2", id6));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id7 + "2", id7));}  
+        if (i%6==0) { 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id10 + "3", id10));}
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id4 + "1", id4));
+
+        if (i%7==0) { 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id2 + "2", id2));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id7 + "1", id7));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id5 + "1", id5));}
+        if (i%8==0) { 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id2 + "2", id2));
+        if (i%9==0){
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id6 + "1", id6));}
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id2 + "2", id2));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id7 + "1", id7));
+        if (i%10==0){ 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id11 + "1", id11));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id9 + "2", id9));}
+        if (i%11==0){ 
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id8 + "2", id8));
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id9 + "1", id9));}
+            camera2.get(i-1).addDetectedObject(new DetectedObject(id1 + "2", id1));}
+
+
+    }
+    
+    /* 
+    for (int i=0; i<12; i++)
+    {
+        System.out.println(camera2.get(i).getTime());
+        for (DetectedObject d: camera2.get(i).getDetectedObject())
+        {
+            System.out.println(d.getID());
+            System.out.println(d.getDescreption());
+        }
+        System.out.println();
+
+    }
+        */
+    return camera2;
+}
+
 
     @Test
-    public void setUp() {
+    public void Test1() {
 
         CountDownLatch latch = new CountDownLatch(0); 
         System.out.println("test has started");
         //create a new object to call the relavant methods
         //creating cameras for the test
         Camera camera = new Camera(1, 1, camera1Test());
-        //Camera camera2 = new Camera(1, 2, r.readCameraData(allData, "camera2"));
         //initialize ints for tests
         int statisticalfolder1 = 0, statisticalfolder2 = 0;
-        //create lists to check thats the DetectedObjectEvent got the excpacted data
         List<StampedDetectedObjects> detectedObjectsList1 = camera.getDetectedObjectsList();
-        //List<StampedDetectedObjects> detectedObjectsList2 = camera2.getDetectedObjectsList();
         
         CameraService cam1 = new CameraService(camera, latch);
-        //CameraService cam2 = new CameraService(camera2, latch);
 
         for (int i = 1; i <= detectedObjectsList1.size(); i++) {
             //create event for the relevant tick
@@ -135,21 +206,33 @@ public class CameraTest {
             }
         }
         System.out.println("yes");
-/* 
+    }
 
-        for (int i = 1; i <= detectedObjectsList2.size(); i++) {
+    @Test
+    public void Test2() {
+
+        CountDownLatch latch = new CountDownLatch(0); 
+        System.out.println("test has started");
+        //create a new object to call the relavant methods
+        //creating cameras for the test
+        Camera camera = new Camera(1, 1, camera2Test());
+        //initialize ints for tests
+        int statisticalfolder1 = 0, statisticalfolder2 = 0;
+        List<StampedDetectedObjects> detectedObjectsList1 = camera.getDetectedObjectsList();
+        CameraService cam1 = new CameraService(camera, latch);
+        for (int i = 1; i <= detectedObjectsList1.size(); i++) {
             //create event for the relevant tick
-            DetectedObjectsEvent event2 = cam2.getcamera().handleTick(i);
+            DetectedObjectsEvent event1 = cam1.getcamera().handleTick(i);
             statisticalfolder1 = StatisticalFolder.getInstance().getNumDetectedObjects();
             //save the list from the relevan tick from the data base
-            List<DetectedObject> tempList = detectedObjectsList2.get(i-1).getDetectedObject();
-            if (event2 != null)
+            List<DetectedObject> tempList = detectedObjectsList1.get(i-1).getDetectedObject();
+            if (event1 != null)
             {
                 //checks there is indeed an error
-                if (event2.getDetectedObject().get(0).getID().equals("ERROR"))
+                if (event1.getDetectedObject().get(0).getID().equals("ERROR"))
                     {
                         boolean findError = false;
-                        for (DetectedObject d : event2.getDetectedObject())
+                        for (DetectedObject d : event1.getDetectedObject())
                         {
                             if (d.getID().equals("ERROR"))
                                 findError = true;
@@ -159,13 +242,13 @@ public class CameraTest {
                     }
                 else{
                     //checks that the StatisticalFolder updated the relevant count
-                    assertTrue(statisticalfolder1 - statisticalfolder2 == event2.getDetectedObject().size());
+                    assertTrue(statisticalfolder1 - statisticalfolder2 == event1.getDetectedObject().size());
                     statisticalfolder2 = statisticalfolder1; 
                     //checks first the sizes are equal 
-                    assertTrue(tempList.size() == event2.getDetectedObject().size());
+                    assertTrue(tempList.size() == event1.getDetectedObject().size());
                     for (int j = 0; j < tempList.size(); j++) {
                         //checks the strings are matched
-                        assertTrue(tempList.get(j).getDescreption().equals(event2.getDetectedObject().get(j).getDescreption()));
+                        assertTrue(tempList.get(j).getDescreption().equals(event1.getDetectedObject().get(j).getDescreption()));
                     }
                 } 
             }
@@ -175,12 +258,8 @@ public class CameraTest {
 
             }
         }
-          */       
-        
-
-        System.out.print("fff");
+        System.out.println("yes");
     }
-
     }
 
 
